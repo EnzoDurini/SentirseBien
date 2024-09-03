6ec36acaf10d2d83579d21253d8737ffd8ecfcd8
Nicolas G Munoz <nicolasgustavomunoz@gmail.com>
2024-09-03 20:31:04 -0300

    solucion router

--- src/app.js
+++ src/app.js
@@ -1,48 +1,71 @@
+// import express from "express";
+// import { __dirname } from "./utils.js";
+// import UserRouter  from "./routes/user.router.js";
+// import UserManager from "./manager/userManager.js";
+
+
+// const userRouter = new UserRouter();
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended:true }));
+
+
+
+// app.use(express.static(__dirname + "/public"));
+
+// //Static
+// app.set('pages', __dirname + "/pages");
+// app.set('views', __dirname + "/views");
+// app.use("/", userRouter)
+
+// app.get("/", (req,res) =>{
+//     res.sendFile( __dirname + "/PAGES/index.html");
+//     console.log(userManager.getUser);
+// });
+// app.get("/aboutUs", (req,res) =>{
+//     res.sendFile( __dirname + "/PAGES/aboutUs.html");
+// });
+// app.get("/employedForm", (req,res) =>{
+//     res.sendFile( __dirname + "/PAGES/employedForm.html");
+// });
+// app.get("/login", (req,res) =>{
+//     res.sendFile( __dirname + "/PAGES/login.html");
+// });
+// app.get("/notices", (req,res) =>{
+//     res.sendFile( __dirname + "/PAGES/notices.html");
+// });
+// app.get("/register", (req,res) =>{
+//     res.sendFile( __dirname + "/PAGES/register.html");
+// });
+// app.get("/services", (req,res) =>{
+//         res.sendFile( __dirname + "/PAGES/services.html");
+// });
+// app.get("/lista", (req,res) =>{
+//     res.sendFile( __dirname + "/PAGES/listaempleados.html");
+// })
+
+// app.use('/users', userRouter);
+
+
+
+// const httpserver = app.listen(8080,()=>console.log("Listening on port 8080"))
+
 import express from "express";
 import { __dirname } from "./utils.js";
-import userRouter from "./routes/user.router.js";
-
+import userRouter from "./routes/user.router.js";  // Cambié UserRouter a userRouter
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
-
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 
-//Static
+// Configuraciones de vistas
 app.set('pages', __dirname + "/pages");
 app.set('views', __dirname + "/views");
-app.get("/", (req,res) =>{
-    res.sendFile( __dirname + "/PAGES/index.html");
-    
-});
-app.get("/aboutUs", (req,res) =>{
-    res.sendFile( __dirname + "/PAGES/aboutUs.html");
-});
-app.get("/employedForm", (req,res) =>{
-    res.sendFile( __dirname + "/PAGES/employedForm.html");
-});
-app.get("/login", (req,res) =>{
-    res.sendFile( __dirname + "/PAGES/login.html");
-});
-app.get("/notices", (req,res) =>{
-    res.sendFile( __dirname + "/PAGES/notices.html");
-});
-app.get("/register", (req,res) =>{
-    res.sendFile( __dirname + "/PAGES/register.html");
-});
-app.get("/services", (req,res) =>{
-        res.sendFile( __dirname + "/PAGES/services.html");
-});
-app.get("/lista", (req,res) =>{
-    res.sendFile( __dirname + "/PAGES/listaempleados.html");
-})
 
-app.use('/users', userRouter);
+// Uso del router
+app.use("/", userRouter);
 
-
-
-const httpserver = app.listen(8080,()=>console.log("Listening on port 8080"))
-
+const httpserver = app.listen(8080, () => console.log("Listening on port 8080"));
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,15 +1,57 @@
+// import { Router } from "express";
+// import UserManager from "../manager/userManager.js";
+// import { __dirname } from "../utils.js";
+
+
+// const router = Router();
+
+// const userManager = new UserManager();
+
+// // router.get('/', async (req, res) =>{
+// //     try {
+// //     const users = await userManager.getUser();
+// //     console.log("Hola Enzo");
+// //     console.log(users);
+// //     res.sendFile( __dirname + "/PAGES/index.html");
+// //     } catch (error) {
+// //         console.log("error");
+        
+// //     }
+// // })
+
+// router.get("/", async (res)=>{
+//     try {
+//     const users = await userManager.getUser();
+//     console.log("Hola Enzo");
+//     console.log(users);
+//     res.sendFile( __dirname + "/PAGES/index.html");
+//     } catch (error) {
+//         console.log("error");
+        
+//     }
+// })
+
+
+// export default router;
+
+
 import { Router } from "express";
 import UserManager from "../manager/userManager.js";
+import { __dirname } from "../utils.js";
 
-
-const userRouter = Router();
-
+const router = Router();
 const userManager = new UserManager();
 
-userRouter.get('/', async (req,res) =>{
-    const users = await userManager.getUser();
-    res.json(users);
-    console.log(users);
-})
+router.get("/", async (req, res) => {  // Agregué `req` como primer parámetro
+    try {
+        const users = await userManager.getUser();
+        console.log("Hola Enzo");
+        console.log(users);
+        res.sendFile(__dirname + "/pages/index.html");  // Corregí la ruta a minúsculas
+    } catch (error) {
+        console.log("error");
+        res.status(500).send("Error interno del servidor");  // Agregué un mensaje de error
+    }
+});
 
-export default userRouter
+export default router;
